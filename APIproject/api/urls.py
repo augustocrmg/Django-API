11298f20636bf0ad468a73adae235b8c6dc1c653
Augusto Durães Camargo <augusto.duraes33@gmail.com>
2021-08-23 15:42:47 -0300

    URLs atualizadas

--- APIproject/api/urls.py
+++ APIproject/api/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
-from .views import ArticleDetails, ArticleList
+from django.urls import path, include
+from rest_framework import routers
+from .views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
 #article_details, article_list
 
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
+
 urlpatterns = [
-    path('articles/', ArticleList.as_view()),
-    path('articles/<int:id>/', ArticleDetails.as_view()),
+    path('', include(router.urls)),
+    #path('articles/', ArticleList.as_view()),
+    #path('articles/<int:id>/', ArticleDetails.as_view()),
     #path('articles/', article_list),
     #path('articles/<int:pk>/', article_details),
 ]
--- APIproject/api/views.py
+++ APIproject/api/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import get_object_or_404, render, HttpResponse
 from rest_framework import serializers
 from .models import Article
 from .serializers import ArticleSerializer
@@ -9,9 +9,85 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import APIView
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework import viewsets
 # Create your views here.
 
 
+class ArticleViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+'''
+class ArticleViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+'''
+
+'''
+class ArticleList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+
+class ArticleDetails(generics.ListCreateAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    lookup_field = 'id'
+
+    def get(self, request, id):
+        return self.retrieve(request, id=id)
+
+    def put(self, request, id):
+        return self.update(request, id=id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id=id)
+
+'''
+'''
 class ArticleList(APIView):
 
     def get(self, request):
@@ -54,7 +130,7 @@ class ArticleDetails(APIView):
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-
+'''
 '''
 @api_view(['GET', 'POST'])
 def article_list(request):
