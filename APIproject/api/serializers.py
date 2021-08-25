d7541db4ebdf629ff6c5540846e669e90e4e9082
Augusto D. Camargo <augusto.duraes33@gmail.com>
2021-08-25 11:17:30 -0300

    geração de tokens corrigida

--- APIproject/api/serializers.py
+++ APIproject/api/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Article
+from rest_framework.authtoken.views import Token
 
 
 class ArticleSerializer(serializers.ModelSerializer):
@@ -20,7 +21,7 @@ class UserSerializer(serializers.ModelSerializer):
             'required': True
         }}
 
-
-def create(self, validated_data):
-    user = User.objects.create_user(**validated_data)
-    return user
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
